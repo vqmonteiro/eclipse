60a6a7a5c852b52e852a564bb73294c6cfca38dd
vqmonteiro <vqmonteiro@hotmail.com>
2019-05-17 09:10:26 -0300

    Update TesteGit.java
    outra pessoa

--- testeGit/src/br/com/red/git/TesteGit.java
+++ testeGit/src/br/com/red/git/TesteGit.java
@@ -6,6 +6,6 @@ public class TesteGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("teste 2");
-		System.out.println("teste2");
+		System.out.println("teste3");
 	}
 }
